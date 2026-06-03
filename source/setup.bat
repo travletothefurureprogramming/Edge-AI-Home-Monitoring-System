07aaddfc7eb9f4087735f06248d1bb6b0691dc99
Gregory Iosifidis <travletothefurureprogramming@gmail.com>
2026-06-03 17:09:42 +0300

    Make better setup.bat and setup.sh

--- source/setup.bat
+++ source/setup.bat
@@ -3,6 +3,13 @@ cd /d "%~dp0"
 
 powershell -ExecutionPolicy Bypass -Command "& '..\.venv\Scripts\Activate.ps1'"
 
+if not exist "files" (
+    mkdir files
+)
+
+powershell -ExecutionPolicy Bypass -Command "irm https://ollama.com/install.ps1 | iex"
+
+type nul > files\.env
 
 pip install customtkinter
 
--- source/setup.sh
+++ source/setup.sh
@@ -4,6 +4,12 @@ cd "$(dirname "$0")"
 
 source ../.venv/bin/activate
 
+mkdir -p files
+
+curl -fsSL https://ollama.com/install.sh | sh
+
+: > files/.env
+
 pip install customtkinter
 
 python3 setup.py
