b7ff7a1d135f24d82c5a7c4538fd07ea10ae068b
Gregory Iosifidis <travletothefurureprogramming@gmail.com>
2026-06-04 13:54:54 +0300

    Make a better setup.bat

--- source/setup.bat
+++ source/setup.bat
@@ -1,10 +1,17 @@
+
 @echo off
 cd /d "%~dp0"
 
 powershell -ExecutionPolicy Bypass -Command "& '..\.venv\Scripts\Activate.ps1'"
 
 
-powershell -ExecutionPolicy Bypass -Command "irm https://ollama.com/install.ps1 | iex"
+where ollama >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Ollama is NOT installed! Running installation script...
+    powershell -ExecutionPolicy Bypass -Command "irm https://ollama.com/install.ps1 | iex"
+) else (
+    echo [Ollama] Is already installed! Skipping installation...
+)
 
 type nul > .env
 
