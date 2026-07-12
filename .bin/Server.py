612cf8c9f23fbeced1842043be6dda59d96cd0b8
travletothefurureprogramming <travletothefurureprogramming@gmail.com>
2026-07-12 19:45:03 +0300

    Fix async connection method in Kasa class to properly await device discovery

--- .bin/Server.py
+++ .bin/Server.py
@@ -950,7 +950,7 @@ class Kasa:
         
         
     async def async_connect(self):
-        await self.device = Discover.discover_single(self.HOST, username=self.username, password=self.password)
+        self.device = await Discover.discover_single(self.HOST, username=self.username, password=self.password)
         await self.device.update()
     
     def connect(self):
